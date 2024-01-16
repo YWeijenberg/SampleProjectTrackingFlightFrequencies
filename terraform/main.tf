73cff76bef86e4df36980727e8601be9e89ad493
YWeijenberg <y.weijenberg0@gmail.com>
2024-01-16 10:28:31 +0100

    feat(tf): add databricks workspace with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,20 @@
-resource "azurerm_resource_group" "TrackingFlightFrequencies" {
-  name     = "TrackingFlightFrequencies"
-  location = "West Europe"
+locals {
+  region = "West Europe"
+  rg_name = "TrackingFlightFrequencies"
 }
 
+resource "azurerm_resource_group" "TrackingFlightFrequencies" {
+  name     = local.rg_name
+  location = local.region
+}
+
+resource "azurerm_databricks_workspace" "example" {
+  name                = "TrackingFlightFrequencies_dbw"
+  resource_group_name = local.rg_name
+  location            = local.region
+  sku                 = "trial"
+
+  tags = {
+    #Put any tags here
+  }
+}
