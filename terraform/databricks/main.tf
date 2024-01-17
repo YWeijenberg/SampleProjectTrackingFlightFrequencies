f8b531e9dcca5df107bb4c7fe484cd5ba199ee08
YWeijenberg <y.weijenberg0@gmail.com>
2024-01-17 12:51:50 +0100

    feat(tf): added data for smallest node type, stopped using cluster policy, implemented singlenode cluster through cluster config.

--- terraform/databricks/main.tf
+++ terraform/databricks/main.tf
@@ -9,23 +9,35 @@ resource "azurerm_databricks_workspace" "databricksworkspace" {
   }
 }
 
+data "databricks_node_type" "smallest" {
+  local_disk = true
+}
+
 # Get the current latest version of databricks and spark
 data "databricks_spark_version" "latest_lts" {
-  depends_on        = [azurerm_databricks_workspace.databricksworkspace]
   long_term_support = true
 }
 
-data "databricks_cluster_policy" "personal" {
-  name = "Personal Compute"
-}
+# data "databricks_cluster_policy" "personal" {
+#   name = "Personal Compute"
+# }
 
 # Create cluster for personal use during project
 resource "databricks_cluster" "personal_cluster" {
+  # policy_id               = data.databricks_cluster_policy.personal.id
   depends_on              = [azurerm_databricks_workspace.databricksworkspace]
   cluster_name            = "personal_cluster"
-  policy_id               = data.databricks_cluster_policy.personal.id
   spark_version           = data.databricks_spark_version.latest_lts.id
-  node_type_id            = "Standard_DS3_v2"
+  node_type_id            = data.databricks_node_type.smallest.id
   autotermination_minutes = 60
-  single_user_name        = "Yannick Weijenberg"
+  data_security_mode      = "SINGLE_USER"
+  single_user_name        = var.user_name
+  num_workers             = 0
+  spark_conf = {
+    "spark.databricks.cluster.profile" = "singleNode"
+    "spark.master"                     = "local[*]"
+  }
+  custom_tags = {
+    "ResourceClass" = "SingleNode"
+  }
 }
--- terraform/databricks/variables.tf
+++ terraform/databricks/variables.tf
@@ -7,3 +7,9 @@ variable "region" {
   type        = string
   description = "Name of resource group where project will be deployed."
 }
+
+variable "user_name" {
+  default     = ""
+  type        = string
+  description = "Email of the user that will use the compute instance"
+}
