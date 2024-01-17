1d0a3321627cf59b8a9c0210a37f3ab2a605037c
YWeijenberg <y.weijenberg0@gmail.com>
2024-01-17 13:19:16 +0100

    feat(tf): organise tf code in databricks module

--- terraform/databricks/main.tf
+++ terraform/databricks/main.tf
@@ -9,19 +9,6 @@ resource "azurerm_databricks_workspace" "databricksworkspace" {
   }
 }
 
-data "databricks_node_type" "smallest" {
-  local_disk = true
-}
-
-# Get the current latest version of databricks and spark
-data "databricks_spark_version" "latest_lts" {
-  long_term_support = true
-}
-
-# data "databricks_cluster_policy" "personal" {
-#   name = "Personal Compute"
-# }
-
 # Create cluster for personal use during project
 resource "databricks_cluster" "personal_cluster" {
   # policy_id               = data.databricks_cluster_policy.personal.id
