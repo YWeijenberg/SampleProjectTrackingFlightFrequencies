0a6ed16947e896ecea3a9a37c25437b256db7379
YWeijenberg <y.weijenberg0@gmail.com>
2024-01-19 13:16:19 +0100

    feat(tf): aded secret for subscription id

--- terraform/data.tf
+++ terraform/data.tf
@@ -10,3 +10,7 @@ data "azuread_user" "my_user" {
   user_principal_name = data.azurerm_key_vault_secret.username.value
 }
 
+data "azurerm_key_vault_secret" "subscriptionId" {
+  name = "subscriptionId"
+  key_vault_id = azurerm_key_vault.keyvault.id
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,5 +11,5 @@ terraform {
 provider "azurerm" {
   features {}
   skip_provider_registration = true
-  subscription_id            = "26f72568-1444-4d55-858f-91155ab83d3d"
+  subscription_id            = data.azurerm_key_vault_secret.subscriptionId
 }
