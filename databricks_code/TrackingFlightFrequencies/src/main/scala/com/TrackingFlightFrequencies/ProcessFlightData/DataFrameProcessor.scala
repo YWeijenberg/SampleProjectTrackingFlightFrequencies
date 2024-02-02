c8b40d80bf9719316f57a9e2eaa0cedab07394b2
YWeijenberg <y.weijenberg0@gmail.com>
2024-02-02 14:04:24 +0100

    feat(scala): add dataframe processor.

---
+++ databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/ProcessFlightData/DataFrameProcessor.scala
@@ -0,0 +1,21 @@
+package com.TrackingFlightFrequencies.ProcessFlightData
+
+import com.TrackingFlightFrequencies.SparkSession.SparkSessionProvider
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.col
+
+object DataFrameProcessor extends SparkSessionProvider {
+  def dataFrameProcessor(df: DataFrame): DataFrame = {
+    val dfFiltered = df.select(
+      col("departure_icao"),
+      col("arrival_icao"),
+      col("departure_scheduled"),
+      col("departure_actual"),
+      col("flight_date"),
+      col("flight_number")
+    )
+    val dfAggregated = dfFiltered.groupBy("arrival_icao").count()
+
+    dfAggregated
+  }
+}
