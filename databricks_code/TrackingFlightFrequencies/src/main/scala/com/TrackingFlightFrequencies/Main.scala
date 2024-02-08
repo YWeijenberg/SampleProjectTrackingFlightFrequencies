84063f4c1c5f04e9e044f019ecaf5c8d9478e4ad
YWeijenberg <y.weijenberg0@gmail.com>
2024-02-08 10:27:20 +0100

    feat(scala): implement sample data to relieve api from requests

--- databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/Main.scala
+++ databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/Main.scala
@@ -3,16 +3,15 @@ package com.TrackingFlightFrequencies
 import com.TrackingFlightFrequencies.DataArchiving.DataFrameArchiver
 import com.TrackingFlightFrequencies.Ingestion.FlightData.{ApiRequest, DataFrameDenester, JsonParser}
 import com.TrackingFlightFrequencies.ProcessFlightData.DataFrameProcessor
+import com.TrackingFlightFrequencies.SampleData.SampleData.sampleData
 import com.TrackingFlightFrequencies.Variables.GlobalVars
 import com.TrackingFlightFrequencies.SparkSession.SparkSessionProvider
-
-// Import for Local Environment Testing
-import com.TrackingFlightFrequencies.SampleData.SampleData.sampleData
 
 object Main extends GlobalVars with SparkSessionProvider {
   def main(args: Array[String]): Unit = {
     // Code for Databricks Environment .jar
-    val jsonString: String = ApiRequest.request(url = url, apiKey = apiKey)
+//    val jsonString: String = ApiRequest.request(url = url, apiKey = apiKey)
+    val jsonString: String = sampleData
     val raw_df = JsonParser.parse(jsonString)
     val flattenedDf = DataFrameDenester.flattenDataFrame(raw_df)
     DataFrameArchiver.writeDataFrameToBlob(
