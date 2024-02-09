752ba5ba1f0d17b4160a487315b611eaec31f170
YWeijenberg <y.weijenberg0@gmail.com>
2024-02-09 09:53:23 +0100

    feat(tf): create queries for visualizations

--- terraform/modules/databricks/queries/main.tf
+++ terraform/modules/databricks/queries/main.tf
@@ -2,13 +2,32 @@ resource "databricks_sql_query" "create_table" {
   data_source_id = var.data_source_id
   name           = "Create table departures_count_{icao}"
   query          = <<-EOT
-            CREATE TABLE IF NOT EXISTS departures_count_EHAM (
-              id BIGINT GENERATED ALWAYS AS IDENTITY,
-              arrival_iata STRING,
-              count INT,
-              flight_date DATE,
-              airport STRING
-            )
-          EOT
+    CREATE TABLE IF NOT EXISTS departures_count_EHAM (
+      id BIGINT GENERATED ALWAYS AS IDENTITY,
+      arrival_iata STRING,
+      count INT,
+      flight_date DATE,
+      airport STRING)
+    EOT
 }
 
+resource "databricks_sql_query" "visualization_dep_by_arr" {
+  data_source_id = var.data_source_id
+  name           = "Query of Departures Count by Arrival Airport"
+  query          = <<-EOT
+    SELECT count, airport FROM departures_count_eham
+    WHERE flight_date = current_date()
+    ORDER BY count DESC
+    EOT
+}
+
+resource "databricks_sql_query" "visualization_dep_over_time" {
+  data_source_id = var.data_source_id
+  name           = "Query of Departure Count over Time"
+  query          = <<-EOT
+    SELECT flight_date, SUM(count) AS total_flights
+    FROM departures_count_eham
+    GROUP BY flight_date
+    ORDER BY flight_date DESC
+    EOT
+}
--- terraform/modules/databricks/queries/output.tf
+++ terraform/modules/databricks/queries/output.tf
@@ -1,3 +1,11 @@
 output "create_table_query_id" {
   value = databricks_sql_query.create_table.id
 }
+
+output "visualization_dep_by_arr_id" {
+  value = databricks_sql_query.visualization_dep_by_arr.id
+}
+
+output "visualization_dep_over_time_id" {
+  value = databricks_sql_query.visualization_dep_over_time.id
+}
