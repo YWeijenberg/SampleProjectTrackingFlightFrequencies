867fb5fa06176452e5b447a9f1ee737bc4c4facc
YWeijenberg <y.weijenberg0@gmail.com>
2024-02-09 09:55:13 +0100

    feat(scala): change apirequest for new api

--- databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/Ingestion/FlightData/ApiRequest.scala
+++ databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/Ingestion/FlightData/ApiRequest.scala
@@ -2,10 +2,13 @@ package com.TrackingFlightFrequencies.Ingestion.FlightData
 
 object ApiRequest {
 
-  def request(url: String, apiKey: String): String = {
+  def request(url: String, apiKey: String, apiHost: String): String = {
     val r = requests.get(
       url = url,
-      params = Map("access_key" -> apiKey)
+      headers = Map(
+        "X-RapidAPI-Key" -> apiKey,
+        "X-RapidAPI-Host" -> apiHost
+      )
     )
     if (r.statusCode == 200) r.text else s"Error: ${r.statusCode}"
   }
