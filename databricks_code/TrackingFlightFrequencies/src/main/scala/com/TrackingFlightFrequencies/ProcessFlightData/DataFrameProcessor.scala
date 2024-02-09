59f7ebeb6a33548f4bb03879f0ca37e36c2e5507
YWeijenberg <y.weijenberg0@gmail.com>
2024-02-09 14:41:21 +0100

    feat(scala): update dataframeprocessor for new api

--- databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/ProcessFlightData/DataFrameProcessor.scala
+++ databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/ProcessFlightData/DataFrameProcessor.scala
@@ -6,26 +6,31 @@ import org.apache.spark.sql.functions.{col, count, to_date}
 import com.TrackingFlightFrequencies.Ingestion.AirportDefinitions.ReadAirportDefinitions.airportDefinitions
 
 object DataFrameProcessor extends SparkSessionProvider {
-  def dataFrameProcessor(df: DataFrame, defPath: String): DataFrame = {
+  def dataFrameProcessor(df: DataFrame,
+                         isDeparture: Boolean = true,
+                         defPath: String): DataFrame = {
     val airportNameIataPairs = airportDefinitions(defPath).select(col("iata"), col("airport"))
 
     val dfFiltered = df.select(
-      col("arrival_iata"),
-      col("departure_scheduled"),
-      col("departure_actual"),
-      col("flight_number"),
-      to_date(col("flight_date"), "yyyy-MM-dd").alias("flight_date"),
+      col("movement_airport_iata").as("airport_iata"),
+      col(s"${if (isDeparture) "departure" else "arrival"}_isCargo").as("isCargo"),
+      col("movement_scheduledTime_local").cast("date").as("flight_date")
     )
 
     // Aggregate and get count
-    val dfAggregated = dfFiltered.groupBy("arrival_iata", "flight_date").agg(count("*").alias("count"))
+    val dfAggregated = dfFiltered.groupBy("airport_iata", "isCargo", "flight_date").agg(count("*").alias("count"))
 
     // Correctly apply casting to integer type
     val dfAggregatedCasted = dfAggregated.withColumn("count", dfAggregated("count").cast("int"))
 
     val dfWithNames = dfAggregatedCasted
-      .join(airportNameIataPairs, dfAggregated("arrival_iata") === airportNameIataPairs("iata"), "left")
-      .select(col("arrival_iata"), col("count"), col("flight_date"), col("airport"))
+      .join(airportNameIataPairs, dfAggregated("airport_iata") === airportNameIataPairs("iata"), "left")
+      .select(col("airport_iata"),
+        col("count"),
+        col("isCargo"),
+        col("flight_date"),
+        col("airport").as("airport_name")
+      )
 
     dfWithNames
   }
