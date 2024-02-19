d30784e25a4c20bff2abff5879568a31b93f844a
YWeijenberg <y.weijenberg0@gmail.com>
2024-02-19 14:27:30 +0100

    doc(scala): remove unnecesary comment

--- databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/DataArchiving/DataFrameArchiver.scala
+++ databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/DataArchiving/DataFrameArchiver.scala
@@ -11,7 +11,7 @@ object DataFrameArchiver extends SparkSessionProvider with GlobalVars {
     val outputDirectory = s"archive/${airportIcao}/${if (isDeparture) "departures" else "arrivals"}"
     val outputPath =
       s"abfss://${containerName}@${storageAccountname}.dfs.core.windows.net/" +
-        s"${outputDirectory}" // Specify your output directory path
+        s"${outputDirectory}"
 
     dataFrame.write.mode("append").parquet(outputPath)
   }
