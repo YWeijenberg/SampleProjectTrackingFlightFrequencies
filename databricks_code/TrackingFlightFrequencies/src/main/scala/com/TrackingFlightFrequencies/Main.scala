e1475d83741a9a631c8c4a87ab2be2aa02cbc4f6
YWeijenberg <y.weijenberg0@gmail.com>
2024-02-19 14:29:08 +0100

    refactor(scala): remove old commented out code and unnecessary imports

--- databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/Main.scala
+++ databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/Main.scala
@@ -3,19 +3,17 @@ package com.TrackingFlightFrequencies
 import com.TrackingFlightFrequencies.DataArchiving.DataFrameArchiver
 import com.TrackingFlightFrequencies.Ingestion.FlightData.{ApiRequest, DataFrameDenester, JsonParser}
 import com.TrackingFlightFrequencies.ProcessFlightData.DataFrameProcessor
-import com.TrackingFlightFrequencies.SampleData.SampleData.sampleData
 import com.TrackingFlightFrequencies.Variables.GlobalVars
 import com.TrackingFlightFrequencies.SparkSession.SparkSessionProvider
-import org.apache.spark.sql.DataFrame
 
 object Main extends GlobalVars with SparkSessionProvider {
   def main(args: Array[String]): Unit = {
-    // Code for Databricks Environment .jar
+
     val jsonString: String = ApiRequest.request(url = url, apiKey = apiKey, apiHost = apiHost)
-//    val raw_df: DataFrame = sampleData
     val raw_df = JsonParser.parse(jsonString)
     val flatDepartures = DataFrameDenester.flattenDepartures(raw_df)
     val flatArrivals = DataFrameDenester.flattenArrivals(raw_df)
+
     DataFrameArchiver.writeDataFrameToBlob(
       dataFrame = flatDepartures,
     )
@@ -26,12 +24,6 @@ object Main extends GlobalVars with SparkSessionProvider {
 
     val processedDepartures = DataFrameProcessor.dataFrameProcessor(df=flatDepartures, defPath = definitionsPath)
     val processedArrivals = DataFrameProcessor.dataFrameProcessor(df=flatArrivals, isDeparture = false, defPath = definitionsPath)
-
-    // Code for Local Environment Testing
-//    val jsonString = sampleData
-//    val raw_df = JsonParser.parse(jsonString)
-//    val flattenedDf = DataFrameDenester.flattenDataFrame(raw_df)
-//    val processedDf = DataFrameProcessor.dataFrameProcessor(df = flattenedDf, definitionsPath = definitionsPath)
 
     processedDepartures.write
       .mode("append")
