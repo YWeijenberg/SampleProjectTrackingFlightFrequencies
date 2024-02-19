64c5a119d102c24a4653c20d0ac129ece0fe1a0d
YWeijenberg <y.weijenberg0@gmail.com>
2024-02-19 14:28:50 +0100

    refactor(scala): remove old commented out code

--- databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/SparkSession/SparkSessionProvider.scala
+++ databricks_code/TrackingFlightFrequencies/src/main/scala/com/TrackingFlightFrequencies/SparkSession/SparkSessionProvider.scala
@@ -4,22 +4,6 @@ import com.TrackingFlightFrequencies.Variables.GlobalVars
 import org.apache.spark.sql.SparkSession
 
 trait SparkSessionProvider extends GlobalVars {
-//  private val defaultConf = new SparkConf()
-//    .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-//    .set("spark.sql.parquet.compression.codec", "snappy")
-//    .set("spark.sql.parquet.mergeSchema", "false")
-//    .set("spark.sql.sources.partitionOverwriteMode", "dynamic")
-//    .set("spark.log.level", "Warn")
-//    .set(s"fs.azure.account.auth.type.${storageAccountname}.dfs.core.windows.net", "SAS")
-//    .set(s"fs.azure.sas.token.provider.type.${storageAccountname}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
-//    .set(s"fs.azure.sas.fixed.token.${storageAccountname}.dfs.core.windows.net", sasToken)
-//
-//  private val conf = {
-//    if (defaultConf.contains("spark.master")) defaultConf
-//    else defaultConf.setMaster("local[*]")
-//  }
-//
-//  val spark: SparkSession = SparkSession.builder.config(conf).getOrCreate()
 
   val spark: SparkSession = SparkSession.builder.getOrCreate()
 
